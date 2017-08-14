fabf0da40efb3a2d08db205e2666ca1d82d86548
alexgriff <alexalexgriffith@gmail.com>
2017-08-14 13:45:06 -0400

    Ready for lecture. Including bad seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+Student.create(name: 'Meryl', email: 'meryl@meryl.com', dob: Date.parse('2017-11-16'))
+Student.create(name: 'Alex', email: 'alex@alex.com', dob: Date.parse('2017-07-02'))
+Student.create(name: 'Alex', email: 'alex@alex.com', dob: Date.parse('2017-07-02'))
+Student.create(name: '')
+Student.create(name: 'asdf')
+Student.create(name: 'Lindsey', email: 'lindsey@lindsey.com', dob: Date.parse('2017-11-16'))
